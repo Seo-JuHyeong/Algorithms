fdf01570929155d0001fed67048af21653ceb72e
Seo-JuHyeong <sjh990721@naver.com>
2023-01-08 02:57:29 +0900

    이전 순열

--- 순열_조합/Baekjoon_10973.java
+++ 순열_조합/Baekjoon_10973.java
@@ -33,19 +33,19 @@ public class Baekjoon_10973 {
 
         // a[j] < a[j-1]를 만족하는 가장 큰 j 찾기
         for (int i = 1; i <= l; i++) {
-            if (a[i-1] <= a[i]) {
+            if (a[i-1] >= a[i]) {
                 j = i;
             }
         }
 
-        // 마지막에 오는 순열인 경우
+        // 처음에 오는 순열인 경우
         if (j == 0) {
             return false;
         }
 
-        // k >= j 이면서 a[j-1] < a[k]를 만족하는 가장 큰 k 찾기
+        // k >= j 이면서 a[j-1] > a[k]를 만족하는 가장 큰 k 찾기
         for (int i = j; i <= l; i++){
-            if (a[i] > a[j-1]) {
+            if (a[i] < a[j-1]) {
                 k = i;
             }
         }
@@ -53,7 +53,7 @@ public class Baekjoon_10973 {
         // a[j-1]과 a[k]를 swap
         swap(a,j-1, k);
 
-        // a[j]부터 순열을 뒤집기 (내림차순 -> 오름차순)
+        // a[j]부터 순열을 뒤집기 (오름차순 -> 내림차순)
         for (int i = j; i < l; i++, l--) {
             swap(a, i, l);
         }
