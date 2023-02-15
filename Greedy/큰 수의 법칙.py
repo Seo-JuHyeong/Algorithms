6274444b9e0b9d8161f63938086bff73f949451f
Seo-JuHyeong <sjh990721@naver.com>
2023-02-15 11:06:33 +0900

    큰 수의 법칙

--- Greedy/큰 수의 법칙.py
+++ Greedy/큰 수의 법칙.py
@@ -9,6 +9,7 @@ data.sort()  # 입력받은 수들 정렬하기
 first = data[n - 1]  # 가장 큰 수
 second = data[n - 2]  # 두 번째로 큰 수
 
+"""
 result = 0
 
 while True:
@@ -21,5 +22,20 @@ while True:
         break
     result += second  # 두 번째로 큰 수를 한 번 더하기
     m -= 1  # 더할 때마다 1씩 빼기
+"""
 
-print(result)
+#  better code
+#  가장 큰 수가 더해지는 횟수 계산
+#  반복되는 수열의 길이 => k + 1
+#  수열이 반복되는 횟수 => M을 (k + 1)로 나눈 몫
+#  큰 수가 등장하는 횟수 => {M을 (k + 1)로 나눈 몫} * K
+count = int(m / (k + 1)) * k
+
+#  M이 (k + 1)로 나누어 떨어지지 않는 경우를 고려
+count += m % (k + 1)  # 나머지만큼 추가로 덧셈
+
+result = 0
+result += (count) * first  # 가장 큰 수 더하기
+result += (m - count) * second  # 두 번째로 큰 수 더하기
+
+print(result)  # 최종 답안 출력
