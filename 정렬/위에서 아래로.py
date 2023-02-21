b66ff206440c00376c5e91e992868721e69188ee
Seo-JuHyeong <sjh990721@naver.com>
2023-02-22 02:45:29 +0900

    178p

--- 정렬/위에서 아래로.py
+++ 정렬/위에서 아래로.py
@@ -1,20 +1,14 @@
-n = 5  # 데이터의 개수 N
-m = 5  # 찾고자 하는 부분합 M
-data = [1, 2, 3, 2, 5]  # 전체 수열
+# N을 입력받기
+n = int(input())
 
-count = 0
-interval_sum = 0
-end = 0
+# N개의 정수를 입력받아 리스트에 저장
+array = []
+for i in range(n):
+    array.append(int(input()))
 
-# start를 차례대로 증가시키며 반복
-for start in range(n):
-    # end를 가능한 만큼 이동시키기
-    while interval_sum < m and end < n:
-        interval_sum += data[end]
-        end += 1
-    # 부분합이 m일 때 카운트 증가
-    if interval_sum == m:
-        count += 1
-    interval_sum -= data[start]
+# 파이썬 기본 정렬 라이브러리를 이용하여 정렬 수행
+array = sorted(array, reverse=True)
 
-print(count)
+# 정렬이 수행된 결과를 출력
+for i in array:
+    print(i, end=' ')
