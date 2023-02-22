412ee767f5a50ddba83a04f0f817f84ada801e84
Seo-JuHyeong <sjh990721@naver.com>
2023-02-23 00:59:27 +0900

    220p

--- Dynamic_Programming/개미 전사.py
+++ Dynamic_Programming/개미 전사.py
@@ -1,6 +1,3 @@
-# i번째 식량창고에 있는 식량의 양 -> k[i]
-# 점화식 : a[i] = max(a[i - 1], a[i - 2] + k[i])
-
 # 정수 N을 입력받기
 n = int(input())
 # 모든 식량 정보 입력받기
@@ -9,6 +6,8 @@ array = list(map(int, input().split()))
 # 앞서 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [0] * 100
 
+# i번째 식량창고에 있는 식량의 양 -> k[i]
+# 점화식 : a[i] = max(a[i - 1], a[i - 2] + k[i])
 # 다이나믹 프로그래밍(Dynamic Programming) 진행 (보텀업)
 d[0] = array[0]
 d[1] = max(array[0], array[1])
